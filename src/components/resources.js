6d731a861e9734999dcd78fa346b1918d18499c8
Ivan Migov <ivan@revimedia.com>
2017-03-31 18:10:15 +0300

    lesson 37

---
+++ src/components/resources.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default ()=> {
+  return(
+    <div>
+      Super Special Recipe
+      <ul>
+        <li>1 Cup Sugar</li>
+        <li> 2 bottles of wine</li>
+        <li>1 spoon of salt</li>
+        <li>love</li>
+      </ul>
+    </div>
+  )
+}
